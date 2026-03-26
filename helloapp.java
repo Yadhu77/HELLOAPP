7aa7d760cec04e33adda2ffe33d65c8ef0caaa93
Yadhu77 <yedhukrishnan8055@gmail.com>
2026-03-26 14:06:55 +0530

    Display Hello With Command Line Argument or Default Message

--- helloapp.java
+++ helloapp.java
@@ -1,6 +1,11 @@
-class public HELLOAPP{
-	public static void main(string [] args){
-	system.out.println(x: "HELLO, World!");
+public class HelloApp {
+	public static void main(String[] args) {
+		String name = "World";
+
+		if (args.length > 0) {
+			name = args[0];
+		}
+		
+		System.out.println("Hello, "+ name + "!");
 	}
 }
-	
